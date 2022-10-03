9b787094c78bbaaefc95c99d11218d47a561c7d7
snakiso <igorchagov@mail.ru>
2022-10-03 08:59:47 +0300

    history

--- src/js/history.js
+++ src/js/history.js
@@ -1,11 +1,13 @@
 let openBtn = document.querySelectorAll('.open-history');
 let closeBtn = document.querySelectorAll('.history-hidden');
 let fullHistory = document.querySelectorAll('.history__full');
+let hoverBox = document.querySelectorAll('.history__box-hover')
 
 //открытие полной истории
 for(let i = 0; i < openBtn.length; i++){
  openBtn[i].addEventListener('click', () => {
   fullHistory[i].classList.add('open')
+  hoverBox[i].classList.add('hidden')
  })
 }
 
@@ -13,5 +15,6 @@ for(let i = 0; i < openBtn.length; i++){
 for (let i = 0; i < openBtn.length; i++) {
  closeBtn[i].addEventListener('click', () => {
   fullHistory[i].classList.remove('open')
+  hoverBox[i].classList.remove('hidden')
  })
 }
